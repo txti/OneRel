7015b20ae455f1b6cd1ed42f2fc6c443d3a0da8a
txti <txti@dataartisan.net>
2025-02-19 16:55:04 -0500

    Upgrade deps and some basic cleanup

--- models/rel_model.py
+++ models/rel_model.py
@@ -1,6 +1,6 @@
-from torch import nn
-from transformers import *
 import torch
+from torch import nn
+from transformers import BertModel
 
 
 class RelModel(nn.Module):
@@ -8,8 +8,12 @@ class RelModel(nn.Module):
         super(RelModel, self).__init__()
         self.config = config
         self.bert_dim = config.bert_dim
-        self.bert_encoder = BertModel.from_pretrained("bert-base-cased", cache_dir='./pre_trained_bert')
-        self.relation_matrix = nn.Linear(self.bert_dim * 3, self.config.rel_num * self.config.tag_size)
+        self.bert_encoder = BertModel.from_pretrained(
+            "bert-base-cased", cache_dir="./pre_trained_bert"
+        )
+        self.relation_matrix = nn.Linear(
+            self.bert_dim * 3, self.config.rel_num * self.config.tag_size
+        )
         self.projection_matrix = nn.Linear(self.bert_dim * 2, self.bert_dim * 3)
 
         self.dropout = nn.Dropout(self.config.dropout_prob)
@@ -21,11 +25,15 @@ class RelModel(nn.Module):
         encoded_text = self.bert_encoder(token_ids, attention_mask=mask)[0]
         return encoded_text
 
-    def triple_score_matrix(self, encoded_text, train = True):
+    def triple_score_matrix(self, encoded_text, train=True):
         # encoded_text: [batch_size, seq_len, bert_dim(768)] 1,2,3
         batch_size, seq_len, bert_dim = encoded_text.size()
         # head: [batch_size, seq_len * seq_len, bert_dim(768)] 1,1,1, 2,2,2, 3,3,3
-        head_representation = encoded_text.unsqueeze(2).expand(batch_size, seq_len, seq_len, bert_dim).reshape(batch_size, seq_len*seq_len, bert_dim)
+        head_representation = (
+            encoded_text.unsqueeze(2)
+            .expand(batch_size, seq_len, seq_len, bert_dim)
+            .reshape(batch_size, seq_len * seq_len, bert_dim)
+        )
         # tail: [batch_size, seq_len * seq_len, bert_dim(768)] 1,2,3, 1,2,3, 1,2,3
         tail_representation = encoded_text.repeat(1, seq_len, 1)
         # [batch_size, seq_len * seq_len, bert_dim(768)*2]
@@ -39,21 +47,22 @@ class RelModel(nn.Module):
         entity_pairs = self.activation(entity_pairs)
 
         # [batch_size, seq_len * seq_len, rel_num * tag_size] -> [batch_size, seq_len, seq_len, rel_num, tag_size]
-        triple_scores = self.relation_matrix(entity_pairs).reshape(batch_size, seq_len, seq_len, self.config.rel_num, self.config.tag_size)
-        
+        triple_scores = self.relation_matrix(entity_pairs).reshape(
+            batch_size, seq_len, seq_len, self.config.rel_num, self.config.tag_size
+        )
+
         if train:
             # [batch_size, tag_size, rel_num, seq_len, seq_len]
-            return triple_scores.permute(0,4,3,1,2)
+            return triple_scores.permute(0, 4, 3, 1, 2)
         else:
             # [batch_size, seq_len, seq_len, rel_num]
-            return triple_scores.argmax(dim = -1).permute(0,3,1,2)
+            return triple_scores.argmax(dim=-1).permute(0, 3, 1, 2)
 
-
-    def forward(self, data, train = True):
+    def forward(self, data, train=True):
         # [batch_size, seq_len]
-        token_ids = data['token_ids']
+        token_ids = data["token_ids"]
         # [batch_size, seq_len]
-        mask = data['mask']
+        mask = data["mask"]
         # [batch_size, seq_len, bert_dim(768)]
         encoded_text = self.get_encoded_text(token_ids, mask)
         encoded_text = self.dropout(encoded_text)
